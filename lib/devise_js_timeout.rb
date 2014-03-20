a7534c97c4f8f07f9307337efc30e0d3f012a20b
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-20 17:54:56 +1100

    Add gem rails

--- lib/devise_js_timeout.rb
+++ lib/devise_js_timeout.rb
@@ -3,14 +3,12 @@ require "devise_js_timeout/version"
 
 require "devise"
 
-module Devise
-  SessionsController.class_eval do
-    def is_expired
-      unless current_user.timedout?(current_user.last_request_at)
-        render json: { status: :ok, message: "User session expired." }
-      else
-        render json: { status: :fail, message: "User is active." }
-      end
+Devise::SessionsController.class_eval do
+  def is_expired
+    unless current_user.timedout?(current_user.last_request_at)
+      render json: { status: :ok, message: "User session expired." }
+    else
+      render json: { status: :fail, message: "User is active." }
     end
   end
 end
