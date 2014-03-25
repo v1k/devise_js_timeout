95065b10901a31b5cd1f3231c043ef443b6599a3
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-26 10:15:19 +1100

    Try to catch session alive with signed_in? helper.

--- config/initializers/session_controller_decorator.rb
+++ config/initializers/session_controller_decorator.rb
@@ -3,8 +3,7 @@ Devise::SessionsController.class_eval do
   logger.debug "Code initialized"
   
   def is_expired
-    logger.debug :last_request_at
-    unless current_user.timedout?(:last_request_at)
+    unless signed_in?
       render json: { status: :ok, message: "User session expired." }
     else
       render json: { status: :fail, message: "User is active." }
