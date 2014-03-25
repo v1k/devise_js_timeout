01a74dfd99cc2a952655614a3610b4eac4ec2492
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-25 14:00:10 +1100

    change routes, try to fix not found issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_scope :users do
-   get "/is_expired", :to => "devise/sessions#is_expired", :as => "session_is_expired"
+   get "is_expired", :to => "sessions#is_expired", :as => "session_is_expired"
  end
 end
