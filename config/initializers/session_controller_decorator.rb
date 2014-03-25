7c8763b73ff4bb4b5960bdd040bac58465f9e222
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-26 10:52:13 +1100

    Small change for debugging. 2

--- config/initializers/session_controller_decorator.rb
+++ config/initializers/session_controller_decorator.rb
@@ -5,8 +5,10 @@ Devise::SessionsController.class_eval do
   def is_expired
     logger.debug "Signed_in" + signed_in?.to_s
     if !signed_in?
+      logger.debug "STATUS OK"
       render json: { status: :ok, message: "User session expired." }
     else
+      logger.debug "STATUS FAIL"
       render json: { status: :fail, message: "User is active." }
     end
   end
