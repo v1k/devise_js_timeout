53cc29a0dfca0596d3cf3e40d502198b5b479cab
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-26 12:02:41 +1100

    Small change for debugging. 3

--- config/initializers/session_controller_decorator.rb
+++ config/initializers/session_controller_decorator.rb
@@ -3,6 +3,8 @@ Devise::SessionsController.class_eval do
   logger.debug "Code initialized"
   
   def is_expired
+    logger.debug current_user.methods.to_s
+    logger.debug user_session.methods.to_s
     render json: { status: :ok, expired: !signed_in?}
   end
 end
--- lib/devise_js_timeout/version.rb
+++ lib/devise_js_timeout/version.rb
@@ -1,3 +1,3 @@
 module DeviseJsTimeout
-  VERSION = "0.1.56"
+  VERSION = "0.1.57"
 end
