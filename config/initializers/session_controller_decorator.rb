62d2177b339259e89de9c6a348bcfd7c7709676f
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-26 12:16:29 +1100

    signed_in contionues the timeout, try user_session.present? instead.

--- config/initializers/session_controller_decorator.rb
+++ config/initializers/session_controller_decorator.rb
@@ -3,8 +3,8 @@ Devise::SessionsController.class_eval do
   logger.debug "Code initialized"
   
   def is_expired
-    logger.debug current_user.methods.to_s
-    logger.debug user_session.methods.to_s
-    render json: { status: :ok, expired: !signed_in?}
+    # logger.debug current_user.methods.to_s
+    # logger.debug user_session.methods.to_s
+    render json: { status: :ok, expired: !user_session.present?}
   end
 end
