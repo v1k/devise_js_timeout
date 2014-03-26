f9df7570474e433994221bee213bd52feaff9901
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-26 13:34:54 +1100

    try to check is js query continues session time

--- config/initializers/session_controller_decorator.rb
+++ config/initializers/session_controller_decorator.rb
@@ -5,6 +5,6 @@ Devise::SessionsController.class_eval do
   def is_expired
     # logger.debug current_user.methods.to_s
     # logger.debug user_session.methods.to_s
-    render json: { status: :ok, expired: !user_session.present?}
+    render json: { status: :ok} #, expired: !user_session.present?}
   end
 end
