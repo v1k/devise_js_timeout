8830871fcf6fbdfd8f699412ec1dfffdcc724184
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-28 13:14:26 +1100

    Try to fix routes patch. 1

--- config/initializers/routes_patch.rb
+++ config/initializers/routes_patch.rb
@@ -2,8 +2,8 @@ class ActionDispatch::Routing::Mapper
   alias_method :old_devise_session, :devise_session
   def devise_session(mapping, controllers)
     old_devise_session(mapping, controllers)
-    resource :session, omly: [], controller: controllers[:sessions], path: "" do
-      get 'is_expired', :to => 'devise/sessions#is_expired'
+    resource :session, only: [], controller: controllers[:sessions], path: "" do
+      get :is_expired, :path '/is_expired'
     end
   end
 end
