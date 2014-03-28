6d844bf24597f077889367f3861bbe268815facf
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-28 13:33:50 +1100

    Try to show pathes.1

--- config/initializers/routes_patch.rb
+++ config/initializers/routes_patch.rb
@@ -1,11 +1,9 @@
 class ActionDispatch::Routing::Mapper
-
-  logger = Logger.new(STDOUT)
-
   alias_method :old_devise_session, :devise_session
 
   def devise_session(mapping, controllers)
     old_devise_session(mapping, controllers)
+    logger = Logger.new(STDOUT)
     logger.debug mapping.path_names[:sign_in]
     resource :session, only: [], controller: controllers[:sessions], path: "" do
       get :is_expired, path: '/is_expired'
