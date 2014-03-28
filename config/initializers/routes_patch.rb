8504b857baf4336c678ab6efdabed2fdf2dbce77
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-28 13:23:57 +1100

    Add debug code

--- config/initializers/routes_patch.rb
+++ config/initializers/routes_patch.rb
@@ -2,6 +2,8 @@ class ActionDispatch::Routing::Mapper
   alias_method :old_devise_session, :devise_session
   def devise_session(mapping, controllers)
     old_devise_session(mapping, controllers)
+    config.logger = Logger.new(STDOUT)
+    logger.debug mapping.path_names[:sign_in]
     resource :session, only: [], controller: controllers[:sessions], path: "" do
       get :is_expired, path: '/is_expired'
     end
