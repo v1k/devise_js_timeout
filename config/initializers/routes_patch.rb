ded100fde7528619e6462d1e2b0ccecae78e7f29
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-28 13:08:47 +1100

    Try to fix routes patch.

--- config/initializers/routes_patch.rb
+++ config/initializers/routes_patch.rb
@@ -1,4 +1,4 @@
-class ActionDispatch::Routing::RouteSet
+class ActionDispatch::Routing::Mapper
   alias_method :old_devise_session, :devise_session
   def devise_session(mapping, controllers)
     old_devise_session(mapping, controllers)
