082b3fdc93846682d352ae5b9665a1e42b306a6d
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-28 12:36:30 +1100

    Add patch for devise routes.

---
+++ config/initializers/routes_patch.rb
@@ -0,0 +1,9 @@
+ActionDispatch::Routing::RouteSet.class_eval do 
+  def devise_session(mapping, controllers)
+    resource :session, omly: [], controller: controllers[:sessions], path: "" do
+      get   :new,        path: mapping.path_names[:sign_in], as: "new"
+      post  :create,     path: mapping.path_names[:sign_in]
+      match :destroy,    path: mapping.path_names[:sign_out], as: "destroy", via: mapping.sign_out_via
+      get   :is_expired, :to => 'devise/sessions#is_expired'
+    end
+end
