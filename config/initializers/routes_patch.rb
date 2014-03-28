2a80a3799d9d16aac3201bde98b23c3c12bbb2d0
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-28 16:55:46 +1100

    Dances with routes. Hack. 2

--- config/initializers/routes_patch.rb
+++ config/initializers/routes_patch.rb
@@ -1,10 +1,8 @@
 class ActionDispatch::Routing::Mapper
   alias_method :old_devise_session, :devise_session
 
-  attr_reader :session_controller
-
   def devise_session(mapping, controllers)
     old_devise_session(mapping, controllers)
-    @session_controller = controllers[:sessions]
+    get 'sessions/is_expired', to: controllers[:sessions] << '#is_expired'
   end
 end
