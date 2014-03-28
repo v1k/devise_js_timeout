bc3eb8cc8c11019a0df2a09e7143ba849ae7e9f4
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-28 12:46:48 +1100

    Fix routes patch. 1

--- config/initializers/routes_patch.rb
+++ config/initializers/routes_patch.rb
@@ -1,10 +1,10 @@
 ActionDispatch::Routing::RouteSet.class_eval do 
   def devise_session(mapping, controllers)
     resource :session, omly: [], controller: controllers[:sessions], path: "" do
-      get   :new,        path: mapping.path_names[:sign_in], as: "new"
-      post  :create,     path: mapping.path_names[:sign_in]
-      match :destroy,    path: mapping.path_names[:sign_out], as: "destroy", via: mapping.sign_out_via
-      get   :is_expired, path: 'devise/sessions#is_expired'
+      get   :new,          path: mapping.path_names[:sign_in], as: "new"
+      post  :create,       path: mapping.path_names[:sign_in]
+      match :destroy,      path: mapping.path_names[:sign_out], as: "destroy", via: mapping.sign_out_via
+      get   '/is_expired', path: 'devise/sessions#is_expired'
     end
   end
 end
